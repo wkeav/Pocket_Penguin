c616077e333ce44b8f9fb204dbaf651f4d93a6c4
CtrlChieftainMsSneha <Sneha.M.Wilson-1@ou.edu>
2025-11-30 20:19:49 -0600

    latest update for calendar

--- backend/penguin_app/urls.py
+++ backend/penguin_app/urls.py
@@ -10,7 +10,6 @@ Author: Astra
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views.user_views import RegisterView, LoginView, CurrentUserView, CurrentUserGameProfile, LogOutView
-from .views.calendar_views import calendar_events, calendar_event_detail, calendar_user_profile
 
 app_name = 'penguin_app'
 
@@ -20,12 +19,8 @@ urlpatterns = [
     path('users/me/', CurrentUserView.as_view(), name='current-user'),
     path('users/me/game-profile/', CurrentUserGameProfile.as_view(), name='current-user-game-profile'),
     
-    path('calendar/events/', calendar_events, name='calendar-events'),
-    path('calendar/events/<int:pk>/', calendar_event_detail, name='calendar-event-detail'),
-    path('calendar/user/profile/', calendar_user_profile, name='calendar-user-profile'),
     # Authentication
     path('auth/token/', LoginView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/revoke/', LogOutView.as_view(), name='token_revoke'),
 ]
-
