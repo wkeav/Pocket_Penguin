57cc5c9861937d73774901b02c5da6a0f73b2085
Astra <astra.k.nguyen05@outlook.com>
2025-12-06 23:30:41 -0600

    Simplify static files config

--- backend/pocket_penguin/settings.py
+++ backend/pocket_penguin/settings.py
@@ -134,15 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# WhiteNoise configuration for static file serving
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
